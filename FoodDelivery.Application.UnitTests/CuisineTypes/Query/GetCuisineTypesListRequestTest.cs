af5f6f57d0f85c67e9a898146a6b4b6786aee52b
Damian Kucharski <kuchar187@gmail.com>
2021-11-20 19:10:12 +0100

    Added unit tests and mocks

---
+++ FoodDelivery.Application.UnitTests/CuisineTypes/Query/GetCuisineTypesListRequestTest.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using FoodDelivery.Application.Contracts.Persistence;
+using FoodDelivery.Application.DTOs.Cuisine;
+using FoodDelivery.Application.Features.CuisineTypes.Handlers.Queries;
+using FoodDelivery.Application.Features.CuisineTypes.Requests.Queries;
+using FoodDelivery.Application.Profiles;
+using FoodDelivery.Application.UnitTests.Mocks;
+using FoodDelivery.Domain.Entities;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FoodDelivery.Application.UnitTests.CuisineTypes.Query
+{
+    public class GetCuisineTypesListRequestTest
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<ICuisineTypeRepository> _mockRepo;
+
+        public GetCuisineTypesListRequestTest()
+        {
+            _mockRepo = MockCuisineTypeRepository.GetCuisineTypeRepository();
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<MappingProfile>();
+            });
+            _mapper = mapperConfig.CreateMapper();
+        }
+
+
+
+
+        [Fact]
+        public async Task GetCuisineTypeListTest()
+        {
+            var handler = new GetCuisineTypesListRequestHandler(_mapper, _mockRepo.Object);
+            var result = await handler.Handle(new GetCuisineTypesListRequest(), CancellationToken.None);
+            result.ShouldBeOfType<List<CuisineTypeDto>>();
+            result.Count.ShouldBe(2);
+        }
+    }
+
+}
---
+++ FoodDelivery.Application.UnitTests/Mocks/MockCuisineTypeRepository.cs
@@ -0,0 +1,43 @@
+﻿using FoodDelivery.Application.Contracts.Persistence;
+using FoodDelivery.Domain.Entities;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDelivery.Application.UnitTests.Mocks
+{
+    public static class MockCuisineTypeRepository
+    {
+        public static Mock<ICuisineTypeRepository> GetCuisineTypeRepository()
+        {
+            var cuisineTypes = new List<CuisineType>
+            {
+                new CuisineType
+                {
+                    Id = 1,
+                    Name = "Polish"
+                },
+                new CuisineType
+                {
+                    Id = 2,
+                    Name = "Georgian"
+                }
+            };
+            var mockRepo = new Mock<ICuisineTypeRepository>();
+            mockRepo.Setup(r => r.GetAll()).ReturnsAsync(cuisineTypes);
+
+            mockRepo.Setup(r => r.Add(It.IsAny<CuisineType>())).ReturnsAsync((CuisineType cuisineType) =>
+            {
+                cuisineTypes.Add(cuisineType);
+                return cuisineType;
+            });
+
+
+            return mockRepo;
+        }
+
+    }
+}
