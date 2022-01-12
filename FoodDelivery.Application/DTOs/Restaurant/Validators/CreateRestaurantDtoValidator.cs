f0a8df7783738a1d4a56f6827a11679f01152d41
Damian Kucharski <kuchar187@gmail.com>
2022-01-12 21:21:30 +0100

    Modified (CreateRestaurantDtoValidator)

--- FoodDelivery.Application/DTOs/Restaurant/Validators/CreateRestaurantDtoValidator.cs
+++ FoodDelivery.Application/DTOs/Restaurant/Validators/CreateRestaurantDtoValidator.cs
@@ -8,19 +8,24 @@ namespace FoodDelivery.Application.DTOs.Restaurant.Validators
 {
     public class CreateRestaurantDtoValidator : AbstractValidator<CreateRestaurantDto>
     {
+        private readonly ICuisineTypeRepository _cuisineTypeRepository;
 
-        public CreateRestaurantDtoValidator()
+        public CreateRestaurantDtoValidator(ICuisineTypeRepository cuisineTypeRepository)
         {
+            _cuisineTypeRepository = cuisineTypeRepository;
 
             Include(new CommonRestaurantDtoValidator());
 
-            RuleForEach(p => p.CuisineTypesIds)
+            RuleFor(p => p.CuisineTypesIds)
                 .NotNull().WithMessage("{PropertyName} is required.");
 
-
-
-
-
+            RuleForEach(p => p.CuisineTypesIds)
+                .MustAsync(async (id, token) =>
+                {
+                    var exist = await _cuisineTypeRepository.Exist(id);
+                    return exist;
+                }).WithMessage("Cuisine type doesn't exist for the given ID ({PropertyValue}).");
+            
         }
     }
 }
